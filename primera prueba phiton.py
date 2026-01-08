9421c8e981c246f654d0aef0c7908107782fd273
marcelocarvajal-ui <marcelo.carvajal@ucb.edu.bo>
2026-01-08 12:27:40 -0400

    primera carga

---
+++ primera prueba phiton.py
@@ -0,0 +1,19 @@
+print("hola, Mundo)
+
+x = 5
+y = 10
+resultado = x + y
+print("Hola, Marcelo el resultado es.")
+print(resultado)
+
+print("------------------------------------------")
+
+print("Calcular el area de un triangulo")
+base = 10
+altura = 57
+area_rectangulo = base * altura 
+print(area_rectangulo)
+
+print("------------------------------------------")
+
+print(7*8)
